611bdfd4035a5ad3c918ca58266aaa12a5931cbd
John-JuniorFofie <johnfofie31@gmail.com>
2025-11-13 14:51:13 +0000

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.tsx 	new file:   src/Components/hero.tsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.tsx 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/Components/hero.tsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Reforming Health Systems for a <br/>Climate-Resilient Future.</h1>
+      <button className="cta-button">research</button>
+    </section>
+  );
+}
+
+export default Hero;
