1016dee4336717260554c30fdb5ca49d698ac1b6
John-JuniorFofie <johnfofie31@gmail.com>
2025-11-14 10:16:19 +0000

    modified:   src/Components/AboutSec.tsx 	modified:   src/index.css

--- src/Components/AboutSec.tsx
+++ src/Components/AboutSec.tsx
@@ -2,7 +2,8 @@ function AboutSec() {
     return (
       <section className="about-section" id="about">
         <div className="about-content">
-            <img src="" alt="" />
+            <div className="image"><img src="public\Images\person-wearing-futuristic-high-tech-virtual-reality-glasses.jpg" alt="" />
+            </div>
             <div className="about-text">
                 <h2>About Us</h2>
                 <p>At TINKAS, we believe healthcare must adapt to the challenges of our time. Our story began with a vision to reform healthcare delivery while recognizing the urgent role of climate change and technology in shaping health outcomes. We are committed to excellence, innovation, equity, sustainability, and collaboration.</p>
