abf029985a7b2a948bc3cc78c21ef9430de830b1
John-JuniorFofie <johnfofie31@gmail.com>
2025-11-14 08:47:57 +0000

    modified:   package-lock.json 	modified:   package.json 	new file:   public/Images/abstract-blurred-background-with-plants-generative-ai.jpg 	new file:   public/Images/earth-day-celebration-concept.jpg 	new file:   public/Images/person-wearing-futuristic-high-tech-virtual-reality-glasses.jpg 	new file:   public/Images/pleased-energetic-group-conservationists-team-up-help-preserve-ecosystems.jpg 	new file:   public/Images/team-volunteers-planting-trees-preserve-natural-ecosystem-forest.jpg 	modified:   src/Components/Header.tsx 	modified:   src/Components/hero.tsx 	modified:   src/index.css 	modified:   vite.config.ts

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,12 +2,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 function Header () {  
     return (
       <header className="header">
-        <div className="logo"><i class="fa-solid fa-bars fa-xl" style="color: #B197FC;"></i></div>
+        <div className="logo text-green-500"><a href="">TINKAS</a></div>
         <nav className="nav">
           <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#services">Services</a>
           <a href="#contact">Contact</a>
+          <i className="fa-solid fa-bars fa-xl" style={{ color: "#000000" }} ></i>
         </nav>
       </header>
     );
--- src/Components/hero.tsx
+++ src/Components/hero.tsx
@@ -1,8 +1,10 @@
 function Hero() {
   return (
-    <section className="hero">
+    <section className="hero ">
+      <div className="hero-content">
       <h1>Reforming Health Systems for a <br/>Climate-Resilient Future.</h1>
       <button className="cta-button">research</button>
+      </div>
     </section>
   );
 }
