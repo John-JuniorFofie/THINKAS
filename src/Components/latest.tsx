0b72f8d3964f186d1a339b8772804a51f8188a94
John-JuniorFofie <johnfofie31@gmail.com>
2025-11-14 13:13:59 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Components/latest.tsx 	modified:   src/index.css

--- src/Components/latest.tsx
+++ src/Components/latest.tsx
@@ -1,14 +1,20 @@
+import { FiArrowRightCircle } from "react-icons/fi";
+import { FiArrowLeftCircle } from "react-icons/fi";
+
 function Latest() { 
     return(
         <section className="latest-section">
             <h4>Finished the latest <br />environmental projects</h4>
             <div className="latest-cards" >  
+                  {/* <div className="latest-item1"><FiArrowLeftCircle className="text-5xl" /></div> */}
                 <div className="latest-item" >
                 <img src="public\Images\team-volunteers-planting-trees-preserve-natural-ecosystem-forest.jpg" alt="" />
                 </div>
                 <div className="latest-item">
                 <img src="public\Images\pleased-energetic-group-conservationists-team-up-help-preserve-ecosystems.jpg" alt="" />
                 </div>
+                <div className="icons">
+                <div className="latest-item1"><FiArrowRightCircle className="text-5xl"/></div></div>
                 </div>
                 </section>
     )
