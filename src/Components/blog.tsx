35717d2ef41380656563e2de935b14bb2602c790
John-JuniorFofie <johnfofie31@gmail.com>
2025-11-14 16:47:52 +0000

    modified:   src/Components/blog.tsx 	modified:   src/index.css

--- src/Components/blog.tsx
+++ src/Components/blog.tsx
@@ -6,20 +6,31 @@ function Blog (){
                 <div className="child">
                     <img src="public\Images\pleased-energetic-group-conservationists-team-up-help-preserve-ecosystems.jpg" alt="" />
                     <p>Why Ghana Needs Climate- <br />Resilient Health Systems</p>
+                 <div className="socials">
+                        <div className="handles"><i className="fa-brands fa-x-twitter" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-facebook" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-instagram" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-linkedin" style={{color: "#08726E"}}></i></div>
+                    </div>
                 </div>
                 <div className="child">
                         <img src="public\Images\pleased-energetic-group-conservationists-team-up-help-preserve-ecosystems.jpg" alt="" />
                     <p>The Role of Digital <br />Innovation in Health Reform</p>
-                
+                 <div className="socials">
+                        <div className="handles"><i className="fa-brands fa-x-twitter" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-facebook" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-instagram" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-linkedin" style={{color: "#08726E"}}></i></div>
+                    </div>
                 </div>
                 <div className="child">
                         <img src="public\Images\pleased-energetic-group-conservationists-team-up-help-preserve-ecosystems.jpg" alt="" />
                     <p>Lessons from COP29 Building <br />Climate-Health Bridges</p>
                     <div className="socials">
-                        <div className="handles"></div>
-                        <div className="handles"></div>
-                        <div className="handles"></div>
-                        <div className="handles"></div>
+                        <div className="handles"><i className="fa-brands fa-x-twitter" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-facebook" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-instagram" style={{color: "#08726E"}}></i></div>
+                        <div className="handles"><i className="fa-brands fa-linkedin" style={{color: "#08726E"}}></i></div>
                     </div>
                 </div>
                  
