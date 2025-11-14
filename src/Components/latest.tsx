7e0e8b33a58b6e8fa5cc63f905cb116f16522a02
John-JuniorFofie <johnfofie31@gmail.com>
2025-11-14 12:37:07 +0000

    modified:   src/Components/latest.tsx 	modified:   src/index.css

--- src/Components/latest.tsx
+++ src/Components/latest.tsx
@@ -4,10 +4,10 @@ function Latest() {
             <h4>Finished the latest <br />environmental projects</h4>
             <div className="latest-cards" >  
                 <div className="latest-item" >
-                    <img src="public\Images\team-volunteers-planting-trees-preserve-natural-ecosystem-forest.jpg" alt="" />
+                <img src="public\Images\team-volunteers-planting-trees-preserve-natural-ecosystem-forest.jpg" alt="" />
                 </div>
                 <div className="latest-item">
-                    <img src="public\Images\team-volunteers-planting-trees-preserve-natural-ecosystem-forest.jpg" alt="" />
+                <img src="public\Images\pleased-energetic-group-conservationists-team-up-help-preserve-ecosystems.jpg" alt="" />
                 </div>
                 </div>
                 </section>
